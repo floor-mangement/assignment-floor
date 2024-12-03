44714600551e10e161d46188a5bed2b8b71d3540
RangikaShalani <rangika2520@gmail.com>
2024-12-03 08:23:39 +0530

    add start comment

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,8 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // want to create recatContex
+    <React.StrictMode>
     <App />
   </React.StrictMode>
 );
